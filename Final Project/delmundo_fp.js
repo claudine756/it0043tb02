743d749d709cc18006440932abcc5d2eca8066dc
claudine756 <154498010+claudine756@users.noreply.github.com>
2024-07-16 22:54:01 +0800

    Update delmundo_fp.js

--- Final Project/delmundo_fp.js
+++ Final Project/delmundo_fp.js
@@ -1,1 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var inputs = document.querySelectorAll('.contactForm input[type="text"], .contactForm input[type="email"], .contactForm input[type="tel"], .contactForm textarea');
 
+  inputs.forEach(function (input) {
+      input.addEventListener('focus', function () {
+          this.setAttribute('data-placeholder', this.getAttribute('placeholder'));
+          this.setAttribute('placeholder', '');
+      });
+
+      input.addEventListener('blur', function () {
+          this.setAttribute('placeholder', this.getAttribute('data-placeholder'));
+      });
+  });
+});
+
+const typed = new Typed('.multiple-text', {
+  strings: ['UI/UX Designer', 'SEO Specialist', 'Full-Stack Developer'],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 1200,
+  loop: true,
+});
+
+function showSidebar() {
+  const sidebar = document.querySelector('#sideBar')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('#sideBar')
+  sidebar.style.display = 'none'
+}
